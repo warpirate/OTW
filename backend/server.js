37c9858af6177143566650db5a73404fd88ef842
codewithT <tulasirammadaka403@gmail.com>
2025-07-21 10:03:40 +0530

    removed subcategories file

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,11 @@ app.use(express.json());
 // Load route
 const pool = require('./config/db');
 const categoryRoute = require('./routes/category_management/categories');
-const subcategoriesRoute = require('./routes/category_management/subcategories');
 const authRoute = require('./routes/authentication/auth');
 const customerRoute = require('./routes/customer_management/customer');
 const baseURL = process.env.BASE_URL1 || '/api';
 
-app.use(`${baseURL}/categories`, categoryRoute, subcategoriesRoute);
+app.use(`${baseURL}/categories`, categoryRoute); 
 app.use(`${baseURL}/auth`, authRoute);
 app.use(`${baseURL}/customer`, customerRoute);
 const PORT = process.env.PORT || 5050;
